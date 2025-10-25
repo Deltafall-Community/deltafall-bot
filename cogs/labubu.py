130330a00c34d47c5a0ef6fe30c1ab3d5221093a
Coleisforrobot <cole07538783669@gmail.com>
2025-10-25 21:34:42 +0100

    Added consumerism

---
+++ cogs/labubu.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class labubu(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="labubu", description="labubu,,, labulabulabubu")
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    async def baby(self, interaction: discord.Interaction):
+        await interaction.response.send_message(content="```         ::.      --.:           \n       :***+*.   ++*#**+         \n      **##-%#*. *#*#+##*-        \n     :###--=%#* ##%===%#*.       \n     ##%--==#%+*#%--==%%#:       \n     #%%-=#=%%%=%#-=%-+##*.      \n    :##%-##=*%%%%%=+#=+%#*       \n    .##%**#+++**##%****#%*       \n    .##****+*#*%%%%#%#%#**       \n    -**%%%*##%#@**##*%#**#+:     \n   =#*######***#%#*#**%***#*-    \n  =#*#####***##%#%##%*+*##**+:   \n :#*##%%--:-----------=+@###**   \n *##%#=-:-----:----===--=%#*##+  \n:+###+-:-.%#--:----%%%---+%*%*+: \n.+*#%=-:*=*##-:----%%%---=%##*+  \n **#%+=:-:%%:-=#%-=+%+=-=-%*#*+  \n  **#%+:-::::-----------=%##**   \n  .**%%%+-:*-------+:-=%%#*#*    \n    -*#####%%#%%%%%%%%#*###:     \n      *#%#%###*##%#%%#%@%#.      \n    #%@%%%#%%%%@%@%#=%%%%%#*     \n   %%%%@%%%%%%%%###=--#%%%###*   \n -#%%%%@#%#%##%%%%##+-#%#%%%%#*  \n.#%%@@%####%%##%%%%#####%%%%###+ \n:-+%%%%##%##%%%%%#%###%##%%#%*== \n-+***###*####%#%%#%##%%#%%#+:   =\n      **####%###%%@%%######-     \n     .####%%#%#%#%%####%%%%:     \n     *##%##########%#@##%##+     \n     **###%#%#@#%%##%##%###*+    \n     *#@%#####@%%%%%#%###%**+    \n     =####%%%%    %##%%%##*      \n      -=-+-*        +=+-=-       \n```")
+
+async def setup(bot):
+    await bot.add_cog(labubu(bot))
