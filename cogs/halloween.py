b40d08b1ef07a37c22325713fb2809019d813ea4
namooTH <methichai3961@gmail.com>
2025-10-29 00:53:04 +0700

    hotfix

--- cogs/halloween.py
+++ cogs/halloween.py
@@ -177,6 +177,9 @@ class HalloweenCommand(commands.Cog):
     @app_commands.command(name="give", description="halloween")
     @app_commands.allowed_installs(guilds=True, users=False)
     async def give(self, interaction: discord.Interaction, candy_name: str, user: discord.User, amount: int):
+        if interaction.user == user:
+            return await interaction.response.send_message(content="you cant give yourself candies")
+        
         candy_name = candy_name.lower()
         if candy_name not in self.vaild_candies:
             return await interaction.response.send_message(content=f"{candy_name} is not a vaild candy name")
